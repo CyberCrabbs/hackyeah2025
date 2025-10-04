101363c9513e3b13b34ac759c22ad25cd3374384
Ivan <ivan.bokuyemskyy@gmail.com>
2025-10-04 20:50:14 +0200

    feat: chat

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,1 +1,26 @@
-import { StreamChat } from "stream-chat";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
+
+export default function Chat() {
+return (<div style={{ position: "relative", height: "500px" }}>
+  <MainContainer>
+    <ChatContainer>
+      <MessageList>
+        <Message
+          model={{
+            message: "Hello my friend",
+            sentTime: "just now",
+            sender: "Joe",
+          }}
+        />
+      </MessageList>
+      <MessageInput placeholder="Type message here" />
+    </ChatContainer>
+  </MainContainer>
+</div>);}
